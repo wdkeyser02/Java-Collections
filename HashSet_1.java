16d3260e915ada1a1f080bd6c190d60732ccb4ae
willydekeyser <ma074172@skynet.be>
2024-10-22 12:01:50 +0200

    Update

---
+++ HashSet_1.java
@@ -0,0 +1,76 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashSet_1 {
+    public static void main(String[] args) {
+        
+        System.out.println("\nJava HashSet\n");
+        System.out.println("Duplicates : ArrayList allows duplicate values while HashSet doesn't allow duplicates values.");
+        System.out.println("Ordering : ArrayList maintains the order of the object in which they are inserted while HashSet is an unordered collection and doesn't maintain any order.");
+
+        HashSet<String> myStringSet = new HashSet<>();
+        // myStringSet.get() (Don't exist!)
+        // myStringSet.sort() (Don't exist!)
+        myStringSet.add("Bill");
+        myStringSet.add("Frank");
+        myStringSet.add("Bill");
+        System.out.println(myStringSet);
+        System.out.println("Size: " + myStringSet.size());
+        myStringSet.forEach(System.out::println);
+
+        HashSet<Person> myPersonSet = new HashSet<>();
+        myPersonSet.add(new Person(1, "Bill", "Gates", 50));
+        myPersonSet.add(new Person(2, "David", "Last", 50));
+        myPersonSet.add(new Person(3, "Bill", "Gates", 50));
+        System.out.println(myPersonSet);
+        myPersonSet.forEach(System.out::println);
+        System.out.println("");
+
+        HashSet<Integer> myIntegerHashSet = new HashSet<>();
+        myIntegerHashSet.add(100);
+        myIntegerHashSet.add(95);
+        myIntegerHashSet.add(2);
+        myIntegerHashSet.add(65);
+        myIntegerHashSet.add(14);
+        myIntegerHashSet.add(1568);
+        System.out.println(myIntegerHashSet);
+        System.out.println("");
+
+        HashSet<String> myHashSet = new HashSet<>();
+        myHashSet.add("A");
+        myHashSet.add("B");
+        myHashSet.add("C");
+        myHashSet.add("D");
+        myHashSet.add("E");
+        myHashSet.add("F");
+
+        System.out.println(myHashSet);
+        System.out.print("Using iterator: ");
+        Iterator<String> iterator = myHashSet.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + ", ");
+        }
+        System.out.println(); 
+        System.out.print("Using enhanced for loop: ");
+        for (String element : myHashSet) {
+            System.out.print(element + ", ");
+        }
+        System.out.println("\n");
+
+        HashSet<String> myHashSet1 = new HashSet<String>();
+        myHashSet1.add("Z");
+        myHashSet1.add("D");
+        myHashSet1.add("C");
+        myHashSet1.add("A");
+        myHashSet1.add("B");
+        myHashSet1.add("F");
+        System.out.println("Initial HashSet " + myHashSet1);
+        myHashSet1.remove("B");
+        System.out.println("After removing element " + myHashSet1);
+        System.out.println("Element AC exists in the Set : " + myHashSet1.remove("AC"));
+        System.out.println("Is myHashSet1 Empty? " + myHashSet1.isEmpty());
+        System.out.println("");
+    }
+
+    private record Person (Integer id, String firstName, String lastName, Integer age) {}
+}
---
+++ HashSet_2.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.concurrent.TimeUnit;
+
+public class HashSet_2 {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        System.out.println("\nJava Array - ArrayList Speedtest!\n");
+        int size = 10000000;
+        Integer value;
+
+        HashSet<Integer> myIntegerHashSet = new HashSet<>();
+        ArrayList<Integer> myIntegerArrayList = new ArrayList<>(size);
+
+        System.out.println("\nSpeed test Add.");
+        long startAdd = System.nanoTime();
+        for(int x = 0; x < size; x++) {
+            myIntegerHashSet.add(x);
+        }
+        long stopAdd = System.nanoTime();
+        for(int x = 0; x < size; x++) {
+            myIntegerArrayList.add(x, x);
+        }
+        long endAdd = System.nanoTime();
+        long intTimeAdd = TimeUnit.NANOSECONDS.toMillis(stopAdd - startAdd);
+        long integerTimeAdd = TimeUnit.NANOSECONDS.toMillis(endAdd - stopAdd);
+        System.out.println("Add HashSet Time: " + intTimeAdd + " ms");
+        System.out.println("Add ArrayList Time: " + integerTimeAdd + " ms");
+        System.out.println("\n");
+
+        System.out.println("\nSpeed test Get.");
+        long startGet = System.nanoTime();
+        for(Integer element : myIntegerHashSet) {
+            value = element;
+        }
+        long stopGet = System.nanoTime();
+        for(int x = 0; x < size; x++) {
+            value = myIntegerArrayList.get(x);
+        }
+        long endGet = System.nanoTime();
+        long intTimeGet = TimeUnit.NANOSECONDS.toMillis(stopGet - startGet);
+        long integerTimeGet = TimeUnit.NANOSECONDS.toMillis(endGet - stopGet);
+        System.out.println("HashSet Get Time: " + intTimeGet + " ms");
+        System.out.println("ArrayList Get Time: " + integerTimeGet + " ms");
+        System.out.println("\n");
+
+    }
+}
