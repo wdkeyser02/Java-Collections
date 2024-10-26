3c8ec6bd691b454556ba888d9a7a7c5725c8e014
willydekeyser <ma074172@skynet.be>
2024-10-26 11:38:35 +0200

    Setup

---
+++ Queue_1.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Queue_1 {
+    public static void main(String[] args) {
+        
+        System.out.println("\nJava Queue\n");
+
+        Queue<Integer> queue = new LinkedList<>();
+    }
+}
